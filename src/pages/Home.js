5ca7553130ef5e490e2e1e881a64f313d31608b0
Victor <vvargas2013@gmail.com>
2025-02-19 12:48:33 +0000

    fix: fixes on counter behaviors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Button, Container, Typography } from "@mui/material";
+import { Backdrop, Button, CircularProgress, Container, Typography } from "@mui/material";
 import Counter from "../components/Counter";
 import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
@@ -11,6 +11,7 @@ const Home = () => {
     const [hitOn, setHitOn] = useState(0);
     const [userId, setUserId] = useState(null);
     const [globalScore, setGlobalScore] = useState({orgyJokes: 0, hitOn: 0});
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -23,21 +24,34 @@ const Home = () => {
 
             const res = await getUserCounters(decodedToken.userId);
             const total = await getGlobalScore();
+
+            console.log(res);
+
+            if (res.counters.length === 1) {
+                if (res.counters[0].category === 'orgyJokes') {
+                    setOrgyJokes(res.counters[0].counter_value);
+                    setHitOn(0);
+                } else {
+                    setHitOn(res.counters[0].counter_value);
+                    setOrgyJokes(0);
+                }
+            } else {
+                const [hitOn, orgyJokes] = res.counters;
+                
+                setOrgyJokes(orgyJokes?.counter_value || 0);
+                setHitOn(hitOn?.counter_value || 0);
+            }
             
-            const [hitOn, orgyJokes] = res.counters;
             const [totalHitOn, totalOrgyJokes] = total.counters;
-
-            console.log(total);
-
-            setOrgyJokes(orgyJokes?.counter_value || 0);
-            setHitOn(hitOn?.counter_value || 0);
-            setGlobalScore({orgyJokes: totalOrgyJokes.total_counter, hitOn: totalHitOn.total_counter});
+            setGlobalScore({orgyJokes: totalOrgyJokes?.total_counter || 0, hitOn: totalHitOn?.total_counter || 0});
         }};
 
         checkToken();
     }, []);
 
     const resetSingleCounters = async () => {
+        setLoading(true);
+
         const res = await resetUserCounters(userId);
 
         if (res.message === 'User counters reset successfully!') {
@@ -46,15 +60,20 @@ const Home = () => {
 
             await incrementGlobalScore();
         }
+
+        setLoading(false);
     };
 
     const incrementGlobalScore = async () => {
         const score = await getGlobalScore();
         const [totalHitOn, totalOrgyJokes] = score.counters;
-        setGlobalScore({orgyJokes: totalOrgyJokes.total_counter, hitOn: totalHitOn.total_counter});
+
+        setGlobalScore({orgyJokes: totalOrgyJokes?.total_counter || 0, hitOn: totalHitOn.total_counter || 0});
     }
 
     const incrementOrgyJokes = async () => {
+        setLoading(true);
+
         const res = await incrementScore(userId, 'orgyJokes', 1);
 
         if (res.message === 'Counter updated successfully!') {
@@ -62,9 +81,13 @@ const Home = () => {
 
             await incrementGlobalScore();
         }
+
+        setLoading(false);
     };
 
     const incrementHitOn = async () => {
+        setLoading(true);
+
         const res = await incrementScore(userId, 'hitOn', 1);
 
         if (res.message === 'Counter updated successfully!') {
@@ -72,12 +95,15 @@ const Home = () => {
 
             await incrementGlobalScore();
         };
+
+        setLoading(false);
     };
 
     const resetAllCounters = async () => {
         const confirmation = prompt('U sure? Imagine losing track of all harrassment we endured!');
 
         if (confirmation?.toLowerCase() === 'yes') {
+            setLoading(true);
 
             const res = await resetGlobalCounters();
     
@@ -87,6 +113,8 @@ const Home = () => {
     
                 await incrementGlobalScore();
             }
+
+            setLoading(false);
         }
     };
 
@@ -97,6 +125,12 @@ const Home = () => {
 
     return (
         <>
+            <Backdrop
+                sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+                open={loading}
+                >
+                <CircularProgress color="inherit" />
+            </Backdrop>
           <Button variant="text" onClick={onLogout} sx={{ position: "absolute", right: 10, top: 10 }}>Logout</Button>
           <Container sx={{ display: 'flex', flexDirection: "column",  alignItems: 'center', justifyContent: 'center', height: '100vh', gap: 6}}>
               <Counter type="Orgy jokes" totalCount={orgyJokes} counterFn={incrementOrgyJokes} />
